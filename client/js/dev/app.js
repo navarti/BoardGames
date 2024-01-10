ffd42b6807f4ffe2bdecfc9c15c2ac3ca303661f
Danylo-Pavlusenko <danil2005oleg@gmail.com>
2024-01-11 01:06:52 +0200

    Added some constraints

--- client/js/dev/app.js
+++ client/js/dev/app.js
@@ -2,12 +2,11 @@ import Chess from './chess/Chess.js'
 
 export default class App {
     constructor(){
-        const gameBoard = document.querySelector('#gameboard')
-        const playerDisplay = document.querySelector('#player')
-        const infoDisplay = document.querySelector('#info-display')
+        const gameBoard = document.querySelector('#gameboard');
+        
 
-        this.game = new Chess()
-        this.game.createBoard(gameBoard)
+        this.game = new Chess();
+        this.game.createBoard(gameBoard, null, 'white');
     }
 }
 
--- client/js/dev/chess/Chess.js
+++ client/js/dev/chess/Chess.js
@@ -1,15 +1,24 @@
 export default class Chess{
     constructor(){
-        this.startPositionId = 0;
+        this.positionFrom = null;
+        this.positionTo = null;
         this.draggedElement = null;
-        
+        this.board = null;
+
+        this.playerDisplay = document.querySelector('#player')
+        this.infoDisplay = document.querySelector('#info-display')
+        this.gameBoardHTML = null;
         // Bind methods
         this.dragStart = this.dragStart.bind(this);
         this.dragOver = this.dragOver.bind(this);
         this.dragDrop = this.dragDrop.bind(this);
     }
 
-    createBoard(gameBoard){
+    createBoard(gameBoardHTML, pieces, playerToMove){
+        this.gameBoardHTML = gameBoardHTML;
+        this.playerToMove = playerToMove;
+        this.updatePlayerDisplay();
+
         const king = '<div class="piece" id="king"><svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M224 0c17.7 0 32 14.3 32 32V48h16c17.7 0 32 14.3 32 32s-14.3 32-32 32H256v48H408c22.1 0 40 17.9 40 40c0 5.3-1 10.5-3.1 15.4L368 400H80L3.1 215.4C1 210.5 0 205.3 0 200c0-22.1 17.9-40 40-40H192V112H176c-17.7 0-32-14.3-32-32s14.3-32 32-32h16V32c0-17.7 14.3-32 32-32zM38.6 473.4L80 432H368l41.4 41.4c4.2 4.2 6.6 10 6.6 16c0 12.5-10.1 22.6-22.6 22.6H54.6C42.1 512 32 501.9 32 489.4c0-6 2.4-11.8 6.6-16z"/></svg></div>';
         const queen = '<div class="piece" id="queen"><svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M256 0a56 56 0 1 1 0 112A56 56 0 1 1 256 0zM134.1 143.8c3.3-13 15-23.8 30.2-23.8c12.3 0 22.6 7.2 27.7 17c12 23.2 36.2 39 64 39s52-15.8 64-39c5.1-9.8 15.4-17 27.7-17c15.3 0 27 10.8 30.2 23.8c7 27.8 32.2 48.3 62.1 48.3c10.8 0 21-2.7 29.8-7.4c8.4-4.4 18.9-4.5 27.6 .9c13 8 17.1 25 9.2 38L399.7 400H384 343.6 168.4 128 112.3L5.4 223.6c-7.9-13-3.8-30 9.2-38c8.7-5.3 19.2-5.3 27.6-.9c8.9 4.7 19 7.4 29.8 7.4c29.9 0 55.1-20.5 62.1-48.3zM256 224l0 0 0 0h0zM112 432H400l41.4 41.4c4.2 4.2 6.6 10 6.6 16c0 12.5-10.1 22.6-22.6 22.6H86.6C74.1 512 64 501.9 64 489.4c0-6 2.4-11.8 6.6-16L112 432z"/></svg></div>';
         const rook = '<div class="piece" id="rook"><svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M32 192V48c0-8.8 7.2-16 16-16h64c8.8 0 16 7.2 16 16V88c0 4.4 3.6 8 8 8h32c4.4 0 8-3.6 8-8V48c0-8.8 7.2-16 16-16h64c8.8 0 16 7.2 16 16V88c0 4.4 3.6 8 8 8h32c4.4 0 8-3.6 8-8V48c0-8.8 7.2-16 16-16h64c8.8 0 16 7.2 16 16V192c0 10.1-4.7 19.6-12.8 25.6L352 256l16 144H80L96 256 44.8 217.6C36.7 211.6 32 202.1 32 192zm176 96h32c8.8 0 16-7.2 16-16V224c0-17.7-14.3-32-32-32s-32 14.3-32 32v48c0 8.8 7.2 16 16 16zM22.6 473.4L64 432H384l41.4 41.4c4.2 4.2 6.6 10 6.6 16c0 12.5-10.1 22.6-22.6 22.6H38.6C26.1 512 16 501.9 16 489.4c0-6 2.4-11.8 6.6-16z"/></svg></div>';
@@ -18,39 +27,72 @@ export default class Chess{
         const knight = '<div class="piece" id="knight"><svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M96 48L82.7 61.3C70.7 73.3 64 89.5 64 106.5V238.9c0 10.7 5.3 20.7 14.2 26.6l10.6 7c14.3 9.6 32.7 10.7 48.1 3l3.2-1.6c2.6-1.3 5-2.8 7.3-4.5l49.4-37c6.6-5 15.7-5 22.3 0c10.2 7.7 9.9 23.1-.7 30.3L90.4 350C73.9 361.3 64 380 64 400H384l28.9-159c2.1-11.3 3.1-22.8 3.1-34.3V192C416 86 330 0 224 0H83.8C72.9 0 64 8.9 64 19.8c0 7.5 4.2 14.3 10.9 17.7L96 48zm24 68a20 20 0 1 1 40 0 20 20 0 1 1 -40 0zM22.6 473.4c-4.2 4.2-6.6 10-6.6 16C16 501.9 26.1 512 38.6 512H409.4c12.5 0 22.6-10.1 22.6-22.6c0-6-2.4-11.8-6.6-16L384 432H64L22.6 473.4z"/></svg></div>';
     
         const startPieces = [
-            rook, knight, bishop, queen, king, bishop, knight, rook,
-            pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
-            '', '', '', '', '', '', '', '',
-            '', '', '', '', '', '', '', '',
-            '', '', '', '', '', '', '', '',
-            '', '', '', '', '', '', '', '',
-            pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
-            rook, knight, bishop, queen, king, bishop, knight, rook
+            [[rook, 'white'], [knight, 'white'], [bishop, 'white'], [king, 'white'], [queen, 'white'], [bishop, 'white'], [knight, 'white'], [rook, 'white']],
+            [[pawn, 'white'], [pawn, 'white'], [pawn, 'white'], [pawn, 'white'], [pawn, 'white'], [pawn, 'white'], [pawn, 'white'], [pawn, 'white']],
+            [['', ''], ['', ''], ['', ''], ['', ''], ['', ''], ['', ''], ['', ''], ['', ''],],
+            [['', ''], ['', ''], ['', ''], ['', ''], ['', ''], ['', ''], ['', ''], ['', ''],],
+            [['', ''], ['', ''], ['', ''], ['', ''], ['', ''], ['', ''], ['', ''], ['', ''],],
+            [['', ''], ['', ''], ['', ''], ['', ''], ['', ''], ['', ''], ['', ''], ['', ''],],
+            [[pawn, 'black'], [pawn, 'black'], [pawn, 'black'], [pawn, 'black'], [pawn, 'black'], [pawn, 'black'], [pawn, 'black'], [pawn, 'black']],
+            [[rook, 'black'], [knight, 'black'], [bishop, 'black'], [king, 'black'], [queen, 'black'], [bishop, 'black'], [knight, 'black'], [rook, 'black']],
         ];
 
-        startPieces.forEach((startPiece, i) => {
-            const square = document.createElement('div');
-            square.innerHTML = startPiece;
-            square.firstChild?.setAttribute('draggable', true);
-            square.classList.add('square');
-            square.classList.add('beige');
-            square.setAttribute('square-id', i);
+        this.board = startPieces;
 
-            const row = Math.floor((63-i)/8)+1;
-            if(row % 2 === 0){
-                square.classList.add(i % 2 === 0 ? 'beige' : 'brown');
-            }
-            else{
-                square.classList.add(i % 2 === 0 ? 'brown' : 'beige') ;
-            }  
-            if(i <= 15){
-                square.firstChild.firstChild.classList.add('black');
-            } 
-            else if(i >= 48){
-                square.firstChild.firstChild.classList.add('white');
-            }
-            
-            gameBoard.append(square);
+        this.board.forEach((row, i) => {
+            row.forEach((startPiece, j) => {
+                const square = document.createElement('div');
+                square.innerHTML = startPiece[0];
+                square.firstChild?.firstChild?.classList.add(startPiece[1]);
+                square.firstChild?.setAttribute('draggable', true);
+                square.classList.add('square');
+                square.setAttribute('square-row', i);
+                square.setAttribute('square-col', j);
+
+                if(i % 2 === 0){
+                    square.classList.add(j % 2 === 0 ? 'brown' : 'beige');
+                }
+                else{
+                    square.classList.add(j % 2 === 0 ? 'beige' : 'brown') ;
+                }  
+        
+                this.gameBoardHTML.append(square);
+            })
+        })
+
+        const allSquares = document.querySelectorAll('#gameboard .square');
+
+        allSquares.forEach(square => {
+            square.addEventListener('dragstart', this.dragStart);
+            square.addEventListener('dragover', this.dragOver);
+            square.addEventListener('drop', this.dragDrop);
+        })
+        this.reverseBoard();
+    }
+
+    reverseBoard(){
+        this.gameBoardHTML.innerHTML = '';
+
+        [...this.board].reverse().forEach((row, i) => {
+            row.reverse().forEach((startPiece, j) => {
+                const square = document.createElement('div');
+                square.innerHTML = startPiece[0];
+                square.setAttribute('draggable', false);
+                square.firstChild?.firstChild?.classList.add(startPiece[1]);
+                square.firstChild?.setAttribute('draggable', true);
+                square.classList.add('square');
+                square.setAttribute('square-row', this.board.length - i - 1);
+                square.setAttribute('square-col', j);
+
+                if(i % 2 === 0){
+                    square.classList.add(j % 2 === 0 ? 'beige' : 'brown');
+                }
+                else{
+                    square.classList.add(j % 2 === 0 ? 'brown' : 'beige') ;
+                }  
+        
+                this.gameBoardHTML.append(square);
+            })
         })
 
         const allSquares = document.querySelectorAll('#gameboard .square');
@@ -63,7 +105,8 @@ export default class Chess{
     }
 
     dragStart(e){
-        this.startPositionId = e.target.parentNode.getAttribute('square-id');
+        //e.preventDefault();
+        this.positionFrom = [e.target.parentNode.getAttribute('square-row'), e.target.parentNode.getAttribute('square-col')];
         this.draggedElement = e.target;
     }
 
@@ -73,8 +116,189 @@ export default class Chess{
 
     dragDrop(e){
         e.stopPropagation();
-        //e.target.parentNode.append(this.draggedElement);
-        e.target.append(this.draggedElement);
+        if(!this.draggedElement.firstChild.classList.contains(this.playerToMove)){
+            this.showInfoToDisplay('It is not your piece');
+            return;
+        }
+        if(e.target.classList.contains('piece')){
+            if(e.target.firstChild.classList.contains(this.playerToMove)){
+                this.showInfoToDisplay('You can not move on cell taken by your other piece');
+                return;
+            }
+            this.positionTo = [e.target.parentNode.getAttribute('square-row'), e.target.parentNode.getAttribute('square-col')];
+            if(!this.checkMove()){
+                this.showInfoToDisplay('Illegal move');
+                return;
+            }
+            e.target.parentNode.append(this.draggedElement);
+            e.target.remove();
+        }
+        else{
+            this.positionTo = [e.target.getAttribute('square-row'), e.target.getAttribute('square-col')];
+            if(!this.checkMove()){
+                this.showInfoToDisplay('Illegal move');
+                return;
+            }
+            e.target.append(this.draggedElement);
+        }
+        this.updatePosition();
+        this.changePlayer();
+    }
+
+    showInfoToDisplay(info){        
+        setTimeout(() => this.infoDisplay.textContent = info, 200);
+    }
+
+    changePlayer(){
+        if(this.playerToMove === 'white'){
+            this.playerToMove = 'black';
+        }
+        else{
+            this.playerToMove = 'white';
+        }
+        this.updatePlayerDisplay();
+    }
+
+    updatePlayerDisplay(){
+        this.playerDisplay.textContent = this.playerToMove;
+    }
+
+    checkMove(){
+        const piece = this.draggedElement.id;
+        this.positionFrom.forEach((pos, i) => {
+            this.positionFrom[i] = parseInt(pos);
+        });
+        this.positionTo.forEach((pos, i) => {
+            this.positionTo[i] = parseInt(pos);
+        });
+        switch(piece){
+            case 'pawn':
+                if(this.playerToMove === 'white'){
+                    //pawn moves forward by 2 cell on its first move
+                    if(this.positionFrom[1] === this.positionTo[1] && this.positionFrom[0] === 1 && this.positionTo[0] === 3){
+                        return true;
+                    }
+
+                    if(this.positionFrom[0] + 1 === this.positionTo[0]){
+                        //eats
+                        if(this.board[this.positionTo[0]][this.positionTo[1]][1] === 'black'){
+                            //a pawn eats b pawn  
+                            if(this.positionFrom[1] === 0 && this.positionTo[1] === 1){
+                                return true;
+                            }
+
+                            //h pawns eats g pawn
+                            if(this.positionFrom[1] === this.board.length - 1 && this.positionTo[1] === this.board.length - 1 - 1){
+                                return true;
+                            }
+
+                            //b-g pawns eat neighbors
+                            if(Math.abs(this.positionFrom[1] - this.positionTo[1]) < 2){
+                                return true;
+                            }
+                        }   
+                        //moves
+                        else{
+                            //pawn moves forward by 1 cell
+                            if(this.positionFrom[1] === this.positionTo[1]){
+                                return true;
+                            }
+                        }
+                    }
+                    return false;
+                }
+                else{
+                    //pawn moves forward by 2 cell on its first move
+                    if(this.positionFrom[1] === this.positionTo[1] && this.positionFrom[0] === 6 && this.positionTo[0] === 4){
+                        return true;
+                    }
+
+                    if(this.positionFrom[0] - 1 === this.positionTo[0]){
+                        //eats
+                        if(this.board[this.positionTo[0]][this.positionTo[1]][1] === 'white'){
+                            //a pawn eats b pawn  
+                            if(this.positionFrom[1] === 0 && this.positionTo[1] === 1){
+                                return true;
+                            }
+
+                            //h pawns eats g pawn
+                            if(this.positionFrom[1] === this.board.length - 1 && this.positionTo[1] === this.board.length - 1 - 1){
+                                return true;
+                            }
+
+                            //b-g pawns eat neighbors
+                            if(Math.abs(this.positionFrom[1] - this.positionTo[1]) < 2){
+                                return true;
+                            }
+                        }   
+                        //moves
+                        else{
+                            //pawn moves forward by 1 cell
+                            if(this.positionFrom[1] === this.positionTo[1]){
+                                return true;
+                            }
+                        }
+                    }
+                    return false;
+                }
+            case 'knight':
+                if(Math.abs(this.positionFrom[1]-this.positionTo[1]) === 2 && Math.abs(this.positionFrom[0]-this.positionTo[0]) === 1){
+                    return true;
+                }
+                if(Math.abs(this.positionFrom[1]-this.positionTo[1]) === 1 && Math.abs(this.positionFrom[0]-this.positionTo[0]) === 2){
+                    return true;
+                }
+                return false;
+            case 'bishop':
+                if(Math.abs(this.positionFrom[1]-this.positionTo[1]) === Math.abs(this.positionFrom[0]-this.positionTo[0])){
+                    // TODO: DOESNT WORK NOW
+                    // const minX = Math.min(this.positionFrom[0], this.positionTo[0]) + 1;  
+                    // const maxX = Math.max(this.positionFrom[0], this.positionTo[0]) - 1;  
+                    // const minY = Math.min(this.positionFrom[1], this.positionTo[1]) + 1;
+                    // const maxY = Math.max(this.positionFrom[1], this.positionTo[1]) - 1;  
+
+                    // for(let i=minX; i<maxX; i++){
+                    //     if(this.board[i][maxY][0] !== ''){
+                    //         return false;
+                    //     }
+                    //     maxY--;
+                    // }
+                    
+                    return true;
+                }
+                return false;
+            case 'rook':
+                if(this.positionFrom[1] !== this.positionTo[1] && this.positionFrom[0] !== this.positionTo[0]){
+                    return false;
+                }
+                for(let i=Math.min(this.positionFrom[1], this.positionTo[1])+1; i<Math.max(this.positionFrom[1], this.positionTo[1]); i++){
+                    if(this.board[i][this.positionFrom[0]][0] !== ''){
+                        return false;
+                    }
+                }
+                for(let i=Math.min(this.positionFrom[0], this.positionTo[0])+1; i<Math.max(this.positionFrom[0], this.positionTo[0]); i++){
+                    if(this.board[i][this.positionFrom[1]][0] !== ''){
+                        return false;
+                    }
+                }
+                return true;
+            case 'king':
+                //write to forbid move under attack
+
+                if(Math.abs(this.positionFrom[1]-this.positionTo[1]) < 2 && Math.abs(this.positionFrom[0]-this.positionTo[0]) < 2){
+                    return true;
+                }
+                return false;
+            default:
+                return true;
+        }
+        return true
+    }
+
+    updatePosition(){
+        this.board[this.positionFrom[0]][this.positionFrom[1]] = ['', ''];
+        this.board[this.positionTo[0]][this.positionTo[1]] = [this.draggedElement.id, this.playerToMove];
     }
 }
+
 
